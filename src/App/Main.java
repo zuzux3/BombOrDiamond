81b69bf7fecba2b6918cf5e633030025432909a6
zuzux3 <zuzannatarazewicz@gmail.com>
2021-10-25 00:05:32 +0200

    add comms

--- src/App/Main.java
+++ src/App/Main.java
@@ -3,6 +3,7 @@ package App;
 public class Main {
 
 	public static void main(String[] args) {
+		//wywo³ywanie konstruktora, aby program móg³ dzia³aæ
 		BombOrDiamond bod = new BombOrDiamond();
 
 	}
